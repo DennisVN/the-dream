49e77efeed52d2edbd4503f39a8e847515cd2374
DennisVN <d.vannieuwenhuyse@gmail.com>
2021-07-28 14:23:50 +0200

    Basic logic with easy if statements works. WIP : Fix displayed message & layout

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>the-dream</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+        <label for="query"> AMOUNT : </label> 
+        <input type="text" name="amount" placeholder="Enter Amount">
+        <input type="submit" name ="convert">
+        <select name="from">
+            <option value="convert from">convert from</option>
+            <option value="MGA">MGA</option>
+            <option value="EUR">EUR</option>
+        </select>
+        <select name="to"> <p>convert to :</p>
+            <option value="convert to">convert to</option>
+            <option value="EUR">EUR</option>
+            <option value="MGA">MGA</option>
+        </select>
+        
+
+    </form>
+<?php
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+if (isset ($_POST ['convert']))
+{
+    $amount = $_POST['amount'];
+    $from = $_POST['from'];
+    $to = $_POST['to'];
+
+    if ($from == 'EUR' AND $to == 'MGA') 
+    {
+        echo "<br><center><b style='background: pink;' Your balance is : >";
+        echo $amount * 4502;
+        echo "</b></center>";
+    }
+    if ($from == 'MGA' AND $to == 'EUR') 
+    {
+        echo "<br><center><b style='background: pink;' Your balance is : >";
+        echo $amount / 4502;
+        echo "</b></center>";
+    }
+}
+?>
+</body>
+</html>
